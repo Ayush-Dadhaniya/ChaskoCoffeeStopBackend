7f76ef07ee4504ba4ef9f8c89a69934d42279e46
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-01-13 12:12:58 +0530

    Backend Deployment

--- manage.py
+++ manage.py
@@ -1,15 +1,14 @@
 import os
 import sys
-from django.core.management import execute_from_command_line
-
-# This is the part that makes it compatible with Vercel.
 from django.core.wsgi import get_wsgi_application
 
+# Vercel requires an app or handler function
 def handler(request, context):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chasko_coffee_stop.settings")
     application = get_wsgi_application()
     return application(request, context)
 
-# Normal Django management tasks.
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chasko_coffee_stop.settings")
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
