0a923798495c5bfd6fdb8021ec25cfe033119cc4
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-01-13 11:48:55 +0530

    Backend Deployment

--- manage.py
+++ manage.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
+# This is the part that makes it compatible with Vercel.
+from django.core.wsgi import get_wsgi_application
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chasko_coffee_stop.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+def handler(request, context):
+    application = get_wsgi_application()
+    return application(request, context)
+
+# Normal Django management tasks.
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chasko_coffee_stop.settings")
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
