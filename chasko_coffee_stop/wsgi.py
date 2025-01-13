5cc37d72f8b0ac909a34dd36f6ddfa07a2c42c14
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-01-13 12:22:43 +0530

    Backend Deployment

--- chasko_coffee_stop/wsgi.py
+++ chasko_coffee_stop/wsgi.py
@@ -1,6 +1,2 @@
-import os
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chasko_coffee_stop.settings')
-
 application = get_wsgi_application()
